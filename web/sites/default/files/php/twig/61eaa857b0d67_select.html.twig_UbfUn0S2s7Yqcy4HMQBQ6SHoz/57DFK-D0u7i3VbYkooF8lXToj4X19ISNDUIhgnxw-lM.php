21b1aae8dda7b890b6c796c805f6841dadd95b9c
shubham <you@example.com>
2022-01-21 18:25:05 +0530

    add a block and place a custom form in to the block 21-01-2022 6:24pm

---
+++ web/sites/default/files/php/twig/61eaa857b0d67_select.html.twig_UbfUn0S2s7Yqcy4HMQBQ6SHoz/57DFK-D0u7i3VbYkooF8lXToj4X19ISNDUIhgnxw-lM.php
@@ -0,0 +1,189 @@
+<?php
+
+use Twig\Environment;
+use Twig\Error\LoaderError;
+use Twig\Error\RuntimeError;
+use Twig\Extension\SandboxExtension;
+use Twig\Markup;
+use Twig\Sandbox\SecurityError;
+use Twig\Sandbox\SecurityNotAllowedTagError;
+use Twig\Sandbox\SecurityNotAllowedFilterError;
+use Twig\Sandbox\SecurityNotAllowedFunctionError;
+use Twig\Source;
+use Twig\Template;
+
+/* themes/bootstrap_barrio/templates/form/select.html.twig */
+class __TwigTemplate_bc621ad4ddd3c3634b39d07444ccbb2e10eebfa75102d9798da18dd9b80b940e extends \Twig\Template
+{
+    private $source;
+    private $macros = [];
+
+    public function __construct(Environment $env)
+    {
+        parent::__construct($env);
+
+        $this->source = $this->getSourceContext();
+
+        $this->parent = false;
+
+        $this->blocks = [
+        ];
+        $this->sandbox = $this->env->getExtension('\Twig\Extension\SandboxExtension');
+        $this->checkSecurity();
+    }
+
+    protected function doDisplay(array $context, array $blocks = [])
+    {
+        $macros = $this->macros;
+        // line 14
+        $context["classes"] = [0 => (((        // line 15
+($context["customtype"] ?? null) == "standard")) ? ("form-control") : ("")), 1 => (((        // line 16
+($context["customtype"] ?? null) == "custom")) ? ("custom-select") : (""))];
+        // line 19
+        echo "
+";
+        // line 20
+        ob_start();
+        // line 21
+        echo "  <select";
+        echo $this->extensions['Drupal\Core\Template\TwigExtension']->escapeFilter($this->env, $this->sandbox->ensureToStringAllowed(twig_get_attribute($this->env, $this->source, ($context["attributes"] ?? null), "addClass", [0 => ($context["classes"] ?? null)], "method", false, false, true, 21), 21, $this->source), "html", null, true);
+        echo ">
+    ";
+        // line 22
+        $context['_parent'] = $context;
+        $context['_seq'] = twig_ensure_traversable(($context["options"] ?? null));
+        foreach ($context['_seq'] as $context["_key"] => $context["option"]) {
+            // line 23
+            echo "      ";
+            if ((twig_get_attribute($this->env, $this->source, $context["option"], "type", [], "any", false, false, true, 23) == "optgroup")) {
+                // line 24
+                echo "        <optgroup label=\"";
+                echo $this->extensions['Drupal\Core\Template\TwigExtension']->escapeFilter($this->env, $this->sandbox->ensureToStringAllowed(twig_get_attribute($this->env, $this->source, $context["option"], "label", [], "any", false, false, true, 24), 24, $this->source), "html", null, true);
+                echo "\">
+          ";
+                // line 25
+                $context['_parent'] = $context;
+                $context['_seq'] = twig_ensure_traversable(twig_get_attribute($this->env, $this->source, $context["option"], "options", [], "any", false, false, true, 25));
+                foreach ($context['_seq'] as $context["_key"] => $context["sub_option"]) {
+                    // line 26
+                    echo "            <option value=\"";
+                    echo $this->extensions['Drupal\Core\Template\TwigExtension']->escapeFilter($this->env, $this->sandbox->ensureToStringAllowed(twig_get_attribute($this->env, $this->source, $context["sub_option"], "value", [], "any", false, false, true, 26), 26, $this->source), "html", null, true);
+                    echo "\"";
+                    echo $this->extensions['Drupal\Core\Template\TwigExtension']->renderVar(((twig_get_attribute($this->env, $this->source, $context["sub_option"], "selected", [], "any", false, false, true, 26)) ? (" selected=\"selected\"") : ("")));
+                    echo ">";
+                    echo $this->extensions['Drupal\Core\Template\TwigExtension']->escapeFilter($this->env, $this->sandbox->ensureToStringAllowed(twig_get_attribute($this->env, $this->source, $context["sub_option"], "label", [], "any", false, false, true, 26), 26, $this->source), "html", null, true);
+                    echo "</option>
+          ";
+                }
+                $_parent = $context['_parent'];
+                unset($context['_seq'], $context['_iterated'], $context['_key'], $context['sub_option'], $context['_parent'], $context['loop']);
+                $context = array_intersect_key($context, $_parent) + $_parent;
+                // line 28
+                echo "        </optgroup>
+      ";
+            } elseif ((twig_get_attribute($this->env, $this->source,             // line 29
+$context["option"], "type", [], "any", false, false, true, 29) == "option")) {
+                // line 30
+                echo "        <option value=\"";
+                echo $this->extensions['Drupal\Core\Template\TwigExtension']->escapeFilter($this->env, $this->sandbox->ensureToStringAllowed(twig_get_attribute($this->env, $this->source, $context["option"], "value", [], "any", false, false, true, 30), 30, $this->source), "html", null, true);
+                echo "\"";
+                echo $this->extensions['Drupal\Core\Template\TwigExtension']->renderVar(((twig_get_attribute($this->env, $this->source, $context["option"], "selected", [], "any", false, false, true, 30)) ? (" selected=\"selected\"") : ("")));
+                echo ">";
+                echo $this->extensions['Drupal\Core\Template\TwigExtension']->escapeFilter($this->env, $this->sandbox->ensureToStringAllowed(twig_get_attribute($this->env, $this->source, $context["option"], "label", [], "any", false, false, true, 30), 30, $this->source), "html", null, true);
+                echo "</option>
+      ";
+            }
+            // line 32
+            echo "    ";
+        }
+        $_parent = $context['_parent'];
+        unset($context['_seq'], $context['_iterated'], $context['_key'], $context['option'], $context['_parent'], $context['loop']);
+        $context = array_intersect_key($context, $_parent) + $_parent;
+        // line 33
+        echo "  </select>
+";
+        echo trim(preg_replace('/>\s+</', '><', ob_get_clean()));
+    }
+
+    public function getTemplateName()
+    {
+        return "themes/bootstrap_barrio/templates/form/select.html.twig";
+    }
+
+    public function isTraitable()
+    {
+        return false;
+    }
+
+    public function getDebugInfo()
+    {
+        return array (  103 => 33,  97 => 32,  87 => 30,  85 => 29,  82 => 28,  69 => 26,  65 => 25,  60 => 24,  57 => 23,  53 => 22,  48 => 21,  46 => 20,  43 => 19,  41 => 16,  40 => 15,  39 => 14,);
+    }
+
+    public function getSourceContext()
+    {
+        return new Source("{#
+/**
+ * @file
+ * Theme override for a select element.
+ *
+ * Available variables:
+ * - attributes: HTML attributes for the select tag.
+ * - options: The option element children.
+ *
+ * @see template_preprocess_select()
+ */
+#}
+{%
+  set classes = [
+    customtype == 'standard' ? 'form-control',
+    customtype == 'custom' ? 'custom-select',
+  ]
+%}
+
+{% spaceless %}
+  <select{{ attributes.addClass(classes) }}>
+    {% for option in options %}
+      {% if option.type == 'optgroup' %}
+        <optgroup label=\"{{ option.label }}\">
+          {% for sub_option in option.options %}
+            <option value=\"{{ sub_option.value }}\"{{ sub_option.selected ? ' selected=\"selected\"' }}>{{ sub_option.label }}</option>
+          {% endfor %}
+        </optgroup>
+      {% elseif option.type == 'option' %}
+        <option value=\"{{ option.value }}\"{{ option.selected ? ' selected=\"selected\"' }}>{{ option.label }}</option>
+      {% endif %}
+    {% endfor %}
+  </select>
+{% endspaceless %}
+", "themes/bootstrap_barrio/templates/form/select.html.twig", "C:\\xampp\\htdocs\\ecommerce\\web\\themes\\bootstrap_barrio\\templates\\form\\select.html.twig");
+    }
+    
+    public function checkSecurity()
+    {
+        static $tags = array("set" => 14, "spaceless" => 20, "for" => 22, "if" => 23);
+        static $filters = array("escape" => 21);
+        static $functions = array();
+
+        try {
+            $this->sandbox->checkSecurity(
+                ['set', 'spaceless', 'for', 'if'],
+                ['escape'],
+                []
+            );
+        } catch (SecurityError $e) {
+            $e->setSourceContext($this->source);
+
+            if ($e instanceof SecurityNotAllowedTagError && isset($tags[$e->getTagName()])) {
+                $e->setTemplateLine($tags[$e->getTagName()]);
+            } elseif ($e instanceof SecurityNotAllowedFilterError && isset($filters[$e->getFilterName()])) {
+                $e->setTemplateLine($filters[$e->getFilterName()]);
+            } elseif ($e instanceof SecurityNotAllowedFunctionError && isset($functions[$e->getFunctionName()])) {
+                $e->setTemplateLine($functions[$e->getFunctionName()]);
+            }
+
+            throw $e;
+        }
+
+    }
+}
